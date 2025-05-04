bfee7da7062f912fd6bd147a20f0be19128804d1
Cristian <CrisRPia>
2025-05-04 01:59:07 -0300

    weather api call seems to work

--- README.md
+++ README.md
@@ -14,3 +14,18 @@ Incluir los campos de [el archivo de ejemplo](./env.example) en .env.
 - [x] Implementar contador
 - [ ] Implementar clima.
 - [ ] Implementar IA.
+
+### Notas
+
+- No utilizo una librería para realizar llamadas a la api de openweather ya que:
+
+    1. La [librería recomendada por openweather](url) no incluye stubs (no es
+       tipada).
+    2. La misma librería creashea al ejecutarse en mi computadora, y no
+       [está mantenida](https://github.com/csparpa/pyowm?tab=readme-ov-file#maintainer-wanted-).
+    3. El resto de librerías que encontré son muy impopulares, por lo que no
+       confío en ellas.
+
+- Persisto estado mediante sqlite para les resulte más sencillo ejecutar el
+  proyecto, ya que python incluye sqlite en su intérprete. Es posible que esto
+  sea redundante si decido añadir Docker al proyecto.
--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
 import os
-import sys
 from telegram.ext import ApplicationBuilder, CommandHandler
-
+from apis.openweather import OpenWeatherClient
 import count_response
 from db.helpers import reinit_if_no_db
 import default_response
 
 TELEGRAM_KEY_PATH = 'TELEGRAM_API_KEY'
+TELEGRAM_TOKEN = os.environ[TELEGRAM_KEY_PATH]
 
 COMMANDS = [
     CommandHandler('start', default_response.default_response),
@@ -15,20 +15,18 @@ COMMANDS = [
 
 
 async def on_bot_start(_):
-    print("Bot iniciado.")
+    print('Bot iniciado.')
     await reinit_if_no_db()
-    print("Base de datos funcional.")
+    print('Base de datos funcional.')
+
 
 def dev():
-    token = os.getenv(TELEGRAM_KEY_PATH)
-
-    if token is None:
-        print(
-            f'Debes incluir el token de tu chat de telegram en {TELEGRAM_KEY_PATH}'
-        )
-        return sys.exit(1)
-
-    app = ApplicationBuilder().token(token).post_init(on_bot_start).build()
+    app = (
+        ApplicationBuilder()
+        .token(TELEGRAM_TOKEN)
+        .post_init(on_bot_start)
+        .build()
+    )
     for command in COMMANDS:
         app.add_handler(command)
 
